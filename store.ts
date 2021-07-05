228333b832f2b68b4b10e7398729ef3552aaf5a4
Linhnef <begin270519@gmail.com>
2021-07-05 21:26:27 +0700

    cant use data in component

---
+++ store.ts
@@ -0,0 +1,32 @@
+import {  configureStore ,combineReducers} from '@reduxjs/toolkit';
+import {counterSlice} from '../features/counter/counter';
+import authSlice from 'features/auth/auth';
+import { cartSlice } from 'features/food/cart';
+import { fecthDataSlice } from 'features/API/fetchSlice';
+
+const reducer = combineReducers({
+    
+})
+
+const store = configureStore({
+  reducer: {
+    counter : counterSlice.reducer,
+    auth : authSlice.reducer,
+    cart : cartSlice.reducer,
+    fetch : fecthDataSlice.reducer
+  }
+});
+
+type RootState = ReturnType<typeof store.getState>;
+
+
+export const counterSelector = (state : RootState) => state.counter.counter;
+export const authSelector = (state : RootState) => state.auth.isLogin;
+export const itemsCartSElector = (state : RootState) => state.cart.items;
+export const totalAmountSelector = (state : RootState) => state.cart.totalAmount
+export const dataFetchSelector = (state : RootState) => state.fetch.data;
+export const loadingFetchSelector = (state : RootState) => state.fetch.loading;
+export const errorFetchSelector = (state : RootState) => state.fetch.error;
+
+
+export default store;
